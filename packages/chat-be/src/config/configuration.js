915296944c626c09ce1605e0bf15cef3cb42ae43
imtangx <939174106@qq.com>
2025-02-26 14:49:22 +0800

    feat(be): 邮箱配置

--- packages/chat-be/src/config/configuration.js
+++ packages/chat-be/src/config/configuration.js
@@ -1,4 +1,3 @@
-import mysql from 'mysql2/promise';
 import dotenv from 'dotenv';
 
 // 根据 NODE_ENV 环境变量选择合适的 .env 文件
@@ -11,9 +10,17 @@ export const databaseConfig = {
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   database: process.env.DB_NAME,
-}
+};
 
 export const websocketConfig = {
   api_url: process.env.API_URL,
   ws_url: process.env.WS_URL,
-}
+};
+
+export const emailConfig = {
+  host: process.env.EMAIL_HOST,
+  port: process.env.EMAIL_PORT,
+  user: process.env.EMAIL_USER,
+  authCode: process.env.EMAIL_AUTH_CODE,
+  from: process.env.EMAIL_FROM,
+};
